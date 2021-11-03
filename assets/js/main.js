4acc288e0cd97c3941eabb24c3babd4cfdc6775b
Dennserrano <dennserrano@gmail.com>
2021-11-03 09:40:58 +0100

    v.1

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+let inputName = document.getElementById("inputName")
+let inputEmail = document.getElementById("inputEmail")
+let inputMassage = document.getElementById("inputMassage")
+
+
+function submitForms() {
+    console.log(inputName.value)
+    console.log(inputEmail.value)
+    console.log(inputMassage.value)
+}
